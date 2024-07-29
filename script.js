4a6cb5776e514d306d33c813d022f00ed79c166c
Olatubosun Sotunde <133935449+Sotunde-Olatubosun@users.noreply.github.com>
2024-07-29 15:11:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -89,9 +89,9 @@ function changeImage(src)
    document.getElementById('mainImg').src = src;
 }
 
-function changeContent(event) {
+function changeContent(section, event) {
     event.preventDefault(); // Prevent the default link behavior
-    function changeContent(section)
+   
     const container1 = document.getElementById('content-contain');
     const container2 = document.getElementById('content-container');
     
